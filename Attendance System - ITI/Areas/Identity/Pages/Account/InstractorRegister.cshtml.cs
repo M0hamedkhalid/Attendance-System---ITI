45021672d4eba46232606ee70e7d3f06580979b2
Mohamed Khaled <mkhaledmohamed07@gmail.com>
2022-05-12 18:25:46 +0200

    Update Register

--- Attendance System - ITI/Areas/Identity/Pages/Account/InstractorRegister.cshtml.cs
+++ Attendance System - ITI/Areas/Identity/Pages/Account/InstractorRegister.cshtml.cs
@@ -148,13 +148,7 @@ namespace Attendance_System___ITI.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 //await _usersManager.CreateAsync(ins,Input.Password);
 
-                Instructor ins = new Instructor();
-                ins.Id = await _userManager.GetUserIdAsync(user);
-                ins.Name = Input.Name;
-                ins.Address = Input.Address;
-                ins.DeptID =Input.DeptId;
-                _context.Add(ins);
-                await _context.SaveChangesAsync();
+                
 
 
                 if (result.Succeeded)
@@ -163,26 +157,33 @@ namespace Attendance_System___ITI.Areas.Identity.Pages.Account
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     await _userManager.AddToRoleAsync(user, "instractor");
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
+                    Instructor ins = new Instructor();
+                    ins.Id = await _userManager.GetUserIdAsync(user);
+                    ins.Name = Input.Name;
+                    ins.Address = Input.Address;
+                    ins.DeptID = Input.DeptId;
+                    _context.Add(ins);
+                    await _context.SaveChangesAsync();
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    //var callbackUrl = Url.Page(
+                    //    "/Account/ConfirmEmail",
+                    //    pageHandler: null,
+                    //    values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                    //    protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
+                    //if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    //{
+                    //    return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    //}
+                    //else
+                    //{
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
-                    }
+                    //}
                 }
                 foreach (var error in result.Errors)
                 {
--- Attendance System - ITI/Areas/Identity/Pages/Account/StudentRegister.cshtml.cs
+++ Attendance System - ITI/Areas/Identity/Pages/Account/StudentRegister.cshtml.cs
@@ -141,18 +141,7 @@ namespace Attendance_System___ITI.Areas.Identity.Pages.Account
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
-                Student std = new Student();
-                std.Id = await _userManager.GetUserIdAsync(user);
-                std.Name = Input.Name;
-                std.Address = Input.Address;
-                std.DeptID =Input.DeptId;
-                std.Faculty = Input.Faculty;
-                std.University = Input.University;
-                std.GraduationGrade = Input.GraduationGrade;
-                std.GraduationYear=Input.GraduationYear;
-                std.Mobile=Input.Mobile;
-                _context.Add(std);
-                await _context.SaveChangesAsync();
+                
 
                 if (result.Succeeded)
                 {
@@ -160,26 +149,38 @@ namespace Attendance_System___ITI.Areas.Identity.Pages.Account
 
                         var userId = await _userManager.GetUserIdAsync(user);
                     await _userManager.AddToRoleAsync(user, "student");
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    //var callbackUrl = Url.Page(
+                    //    "/Account/ConfirmEmail",
+                    //    pageHandler: null,
+                    //    values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                    //    protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                    //if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    //{
+                    //    return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    //}
+                    //else
+                    //{
+                    Student std = new Student();
+                    std.Id = userId;
+                    std.Name = Input.Name;
+                    std.Address = Input.Address;
+                    std.DeptID = Input.DeptId;
+                    std.Faculty = Input.Faculty;
+                    std.University = Input.University;
+                    std.GraduationGrade = Input.GraduationGrade;
+                    std.GraduationYear = Input.GraduationYear;
+                    std.Mobile = Input.Mobile;
+                    _context.Add(std);
+                    await _context.SaveChangesAsync();
+                    await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
-                    }
+                    //}
                 }
                 foreach (var error in result.Errors)
                 {
