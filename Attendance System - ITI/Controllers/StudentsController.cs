aee0c4468321a174d412a2199dfbaace8236e08a
ibrahim21399 <ibrahimmohamed21399@gmail.com>
2022-05-15 06:38:29 +0200

    some editing

--- Attendance System - ITI/Controllers/StudentsController.cs
+++ Attendance System - ITI/Controllers/StudentsController.cs
@@ -26,6 +26,7 @@ namespace Attendance_System___ITI.Controllers
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Students.Include(s => s.Credential).Include(s => s.Department);
+            ViewData["DeptID"] = new SelectList(_context.Departments, "Id", "Name");
             return View(await applicationDbContext.ToListAsync());
         }
 
