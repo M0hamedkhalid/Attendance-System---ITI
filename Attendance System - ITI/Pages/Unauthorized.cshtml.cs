fb711624829e3e964df258d42252e9afafbd32bb
NadaNabil3 <95531216+NadaNabil3@users.noreply.github.com>
2022-05-20 21:18:32 +0200

    Error & Unauthorized & forbidden pages & about us

---
+++ Attendance System - ITI/Pages/Unauthorized.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Attendance_System___ITI.Pages
+{
+    public class UnauthorizedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
